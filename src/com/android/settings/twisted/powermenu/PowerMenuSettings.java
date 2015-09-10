ff3281c90bf8fc79f234e9702361a26f497596e2
Alexander Martinz <eviscerationls@gmail.com>
2015-09-09 18:41:45 -0700

    On-The-Go Mode (2/2)
    Change-Id: I6308fbbadac7816bf58f3c8027ed1c738d65f3db
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    (cherry picked from commit 87ed53a572784e736add5ac773defff3126a08e1)
    
    # Conflicts:
    #	res/values/custom_strings.xml
    #	res/xml/powermenu_settings.xml
    #	src/com/android/settings/twisted/PowerMenuSettings.java

--- src/com/android/settings/twisted/powermenu/PowerMenuSettings.java
+++ src/com/android/settings/twisted/powermenu/PowerMenuSettings.java
@@ -18,12 +18,14 @@
 package com.android.settings.twisted.powermenu;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.UserInfo;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
@@ -39,8 +41,12 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PowerMenuSettings extends SettingsPreferenceFragment {
+public class PowerMenuSettings extends SettingsPreferenceFragment
+         implements OnPreferenceChangeListener {
 
+    private static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
+
+    private SwitchPreference mPowerPref;
     private SwitchPreference mRebootPref;
     private SwitchPreference mScreenshotPref;
     private SwitchPreference mAirplanePref;
@@ -51,6 +57,7 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
     private SwitchPreference mVoicePref;
     private SwitchPreference mBugReportPref;
     private SwitchPreference mSilentPref;
+    private SwitchPreference mOnTheGoPowerMenu;
 
     Context mContext;
     private ArrayList<String> mLocalUserConfig = new ArrayList<String>();
@@ -67,6 +74,11 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
         mAvailableActions = getActivity().getResources().getStringArray(
                 R.array.power_menu_actions_array);
         mAllActions = PowerMenuConstants.getAllActions();
+
+        mOnTheGoPowerMenu = (SwitchPreference) findPreference(POWER_MENU_ONTHEGO_ENABLED);
+        mOnTheGoPowerMenu.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_ONTHEGO_ENABLED, 0) == 1));
+        mOnTheGoPowerMenu.setOnPreferenceChangeListener(this);
 
         for (String action : mAllActions) {
             // Remove preferences not present in the overlay
@@ -160,6 +172,17 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
         }
 
         updatePreferences();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mOnTheGoPowerMenu) {
+            boolean value = ((Boolean)newValue).booleanValue();
+            Settings.System.putInt(getContentResolver(), Settings.System.POWER_MENU_ONTHEGO_ENABLED, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
