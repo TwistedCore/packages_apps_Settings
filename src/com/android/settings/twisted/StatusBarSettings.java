2c36e45f77d39893cc9ee18f1fce5d3aa4379c9a
Steve Kondik <steve@cyngn.com>
2015-11-14 22:12:09 -0500

    Notification Count in Statusbar [2/2]
    * Also fix buttons vector
    
    Change-Id: I55f09dc8c7ac003a69f75cac502d01e1ad44190e
    
    # Conflicts:
    #	res/values/strings.xml
    #	res/xml/statusbar.xml

--- src/com/android/settings/twisted/StatusBarSettings.java
+++ src/com/android/settings/twisted/StatusBarSettings.java
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2015-2016 The Dirty Unicorns Project
  * Copyright (C) 2016 The Pure Nexus Project
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.settings.twisted;
 
 import android.content.Context;
@@ -44,16 +45,46 @@ import com.android.settings.Utils;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-public class StatusBarSettings extends SettingsPreferenceFragment {
+public class StatusbarNotifications extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+    private SwitchPreference mEnableNC;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.statusbar_settings);
+
+        addPreferencesFromResource(R.xml.statusbar_notifications);
+
+        final PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mEnableNC = (SwitchPreference) findPreference(STATUS_BAR_NOTIF_COUNT);
+        mEnableNC.setOnPreferenceChangeListener(this);
+        int EnableNC = Settings.System.getInt(getContentResolver(),
+                STATUS_BAR_NOTIF_COUNT, 0);
+        mEnableNC.setChecked(EnableNC != 0);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.TWISTED;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if  (preference == mEnableNC) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), STATUS_BAR_NOTIF_COUNT,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
