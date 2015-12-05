24a539dd5d3430dda781ce9dd8db919a9b48ace0
Nick Doherty <nickdoh81@gmail.com>
2015-12-05 13:24:57 +0100

    Forward port 'Swap volume buttons' (3/3)
    Conflicts:
    	res/values/strings.xml
    	res/xml/navigation.xml
    
    Change-Id: Ib31ece5073a757c079a8cab41a613cafd00c8326

--- src/com/android/settings/twisted/VolumeRockerSettings.java
+++ src/com/android/settings/twisted/VolumeRockerSettings.java
@@ -15,24 +15,72 @@
  */
 package com.android.settings.twisted;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.content.Context;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
+import android.os.Vibrator;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
-import com.android.internal.logging.MetricsLogger;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
-public class VolumeRockerSettings extends SettingsPreferenceFragment {
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class VolumeRockerSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    // volume rocker reorient
+    private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+    private SwitchPreference mSwapVolumeButtons;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.volumerocker_settings);
+
+        final ContentResolver resolver = getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources res = getResources();
+
+        // volume rocker reorient
+        mSwapVolumeButtons = (SwitchPreference) findPreference(SWAP_VOLUME_BUTTONS);
+        mSwapVolumeButtons.setOnPreferenceChangeListener(this);
+        int swapVolumeButtons = Settings.System.getInt(getContentResolver(),
+                SWAP_VOLUME_BUTTONS, 0);
+        mSwapVolumeButtons.setChecked(swapVolumeButtons != 0);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsLogger.TWISTED_SETTINGS;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mSwapVolumeButtons) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), SWAP_VOLUME_BUTTONS,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
