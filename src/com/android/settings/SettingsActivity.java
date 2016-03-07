3e02239dac81d0dc7838a3dbe73513d652636645
beanstown106 <nbenis106@gmail.com>
2016-03-07 08:26:41 -0500

    PureAmbient display options (2/2) *This is a rehash of parts of slims and parts of omni's ambient display mods *Added my own little twist on some of the things *Moved all options into a seperate ambient display fragment
    Features:
    *default/user config switch
    *Fade in duration
    *Fade out duration
    *Visibility duration
    *Option to disable pulse on notifications
    *Option to disable pulse with pickup sensor
    
    Credits:
    Maxwen(omni)
    Squadzone(omni)
    Dankoman(slim)
    
    Squash with:
    PureAmbient: cleanup for non pickup sensor
    Point Amibent display tile longpress action to it's own settings
    
    Change-Id: I328b4403151a219605de911f30f3cac7ffa7a915
    
    # Conflicts:
    #	res/values/custom_arrays.xml
    #	res/values/custom_strings.xml
    #	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -93,6 +93,7 @@ import com.android.settings.inputmethod.SpellCheckersSettings;
 import com.android.settings.inputmethod.UserDictionaryList;
 import com.android.settings.localepicker.LocaleListEditor;
 import com.android.settings.location.LocationSettings;
+import com.android.settings.nexus.AmbientSettings;
 import com.android.settings.nfc.AndroidBeam;
 import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.notification.AppNotificationSettings;
@@ -348,6 +349,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
             TwistedCoreSettings.class.getName(),
+            AmbientSettings.class.getName()
     };
 
 
