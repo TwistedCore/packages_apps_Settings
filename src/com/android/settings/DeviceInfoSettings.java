191de7c132cd8ce06d7a4858cb0fb0c818202558
aow1980 <wilkes.allen@gmail.com>
2016-09-10 17:07:59 -0400

    Twisted Version Info

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -67,6 +67,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_VENDOR_VERSION = "vendor_version";
+    private static final String KEY_TWISTED_VERSION = "twisted_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -124,6 +125,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(DeviceInfoUtils.getFormattedKernelVersion());
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        setValueSummary(KEY_TWISTED_VERSION, "ro.twisted.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
