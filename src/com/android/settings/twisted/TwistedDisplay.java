07740d05036f8b8dec327035610144be56d9bf60
Sang Tea Park <pastime1971@gmail.com>
2016-09-12 20:13:56 -0400

    Add option to disable scrolling cache [2/2]
    Change-Id: I5e6b06a2abd1238f720b55209a54619fc1e3cdd4
    
    # Conflicts:
    #	res/xml/disp_settings.xml

--- src/com/android/settings/twisted/TwistedDisplay.java
+++ src/com/android/settings/twisted/TwistedDisplay.java
@@ -15,10 +15,11 @@
  */
 package com.android.settings.twisted;
 
+import android.content.Context;
 import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.os.Vibrator;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
@@ -43,16 +44,44 @@ import com.android.settings.Utils;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-public class TwistedDisplay extends SettingsPreferenceFragment {
+public class TwistedDisplay extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
+    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
+    private static final String SCROLLINGCACHE_DEFAULT = "1";
+
+    private ListPreference mScrollingCachePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.disp_settings);
+
+
+        mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
+        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
+                SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
+        mScrollingCachePref.setOnPreferenceChangeListener(this);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.TWISTED;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+         if (preference == mScrollingCachePref) {
+            if (newValue != null) {
+                SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, (String)newValue);
+            return true;
+            }
+        }
+        return false;
     }
 }
