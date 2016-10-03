f97293a805abf2f7bbcdb116a4f9a5e192c7bfca
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-10-03 10:24:18 +0800

    QS: Custom rows/columns, (2/2)
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    # Conflicts:
    #	res/values/custom_arrays.xml
    #	res/xml/quick_settings.xml
    #	src/com/android/settings/benzo/QuickSettings.java

--- src/com/android/settings/twisted/StatusBarSettings.java
+++ src/com/android/settings/twisted/StatusBarSettings.java
@@ -34,6 +34,7 @@ import android.content.DialogInterface;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
@@ -50,14 +51,31 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
     private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
     private static final String KEY_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
 
+    private static final String PREF_CAT_LANDSCAPE =
+            "qs_cat_landscape";
+    private static final String PREF_ROWS_PORTRAIT =
+            "qs_rows_portrait";
+    private static final String PREF_COLUMNS_PORTRAIT =
+            "qs_columns_portrait";
+    private static final String PREF_ROWS_LANDSCAPE =
+            "qs_rows_landscape";
+    private static final String PREF_COLUMNS_LANDSCAPE =
+            "qs_columns_landscape";
+
     private SwitchPreference mEnableNC;
     private ListPreference mSysuiQqsCount;
+
+    private ListPreference mRowsPortrait;
+    private ListPreference mColumnsPortrait;
+    private ListPreference mRowsLandscape;
+    private ListPreference mColumnsLandscape;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.statusbar_settings);
+        ContentResolver resolver = getActivity().getContentResolver();
 
         final PreferenceScreen prefSet = getPreferenceScreen();
         final ContentResolver resolver = getActivity().getContentResolver();
@@ -68,14 +86,44 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
                 STATUS_BAR_NOTIF_COUNT, 0);
         mEnableNC.setChecked(EnableNC != 0);
 
+
         mSysuiQqsCount = (ListPreference) findPreference(KEY_SYSUI_QQS_COUNT);
         if (mSysuiQqsCount != null) {
            mSysuiQqsCount.setOnPreferenceChangeListener(this);
-           int SysuiQqsCount = Settings.Secure.getInt(resolver,
+           int SysuiQqsCount = Settings.Secure.getInt(getContentResolver(),
                     Settings.Secure.QQS_COUNT, 5);
            mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
         }
+
+        mRowsPortrait = (ListPreference) findPreference(PREF_ROWS_PORTRAIT);
+        int rowsPortrait = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_ROWS_PORTRAIT, 3);
+        mRowsPortrait.setValue(String.valueOf(rowsPortrait));
+        mRowsPortrait.setSummary(mRowsPortrait.getEntry());
+        mRowsPortrait.setOnPreferenceChangeListener(this);
+
+        mColumnsPortrait = (ListPreference) findPreference(PREF_COLUMNS_PORTRAIT);
+        int columnsPortrait = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_COLUMNS_PORTRAIT, 5);
+        mColumnsPortrait.setValue(String.valueOf(columnsPortrait));
+        mColumnsPortrait.setSummary(mColumnsPortrait.getEntry());
+        mColumnsPortrait.setOnPreferenceChangeListener(this);
+
+        mRowsLandscape = (ListPreference) findPreference(PREF_ROWS_LANDSCAPE);
+        int rowsLandscape = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_ROWS_LANDSCAPE, 2);
+        mRowsLandscape.setValue(String.valueOf(rowsLandscape));
+        mRowsLandscape.setSummary(mRowsLandscape.getEntry());
+        mRowsLandscape.setOnPreferenceChangeListener(this);
+
+        mColumnsLandscape = (ListPreference) findPreference(PREF_COLUMNS_LANDSCAPE);
+        int columnsLandscape = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_COLUMNS_LANDSCAPE, 5);
+        mColumnsLandscape.setValue(String.valueOf(columnsLandscape));
+        mColumnsLandscape.setSummary(mColumnsLandscape.getEntry());
+        mColumnsLandscape.setOnPreferenceChangeListener(this);
+  
     }
 
     @Override
@@ -90,19 +138,47 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+       int intValue;
+       int index;
         if  (preference == mEnableNC) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getContentResolver(), STATUS_BAR_NOTIF_COUNT,
                     value ? 1 : 0);
             return true;
        } else if (preference == mSysuiQqsCount) {
-            String SysuiQqsCount = (String) newValue;
+            String SysuiQqsCount = (String) objValue;
             int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
-            Settings.Secure.putInt(resolver, Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
-            int SysuiQqsCountIndex = mSysuiQqsCount
-                    .findIndexOfValue(SysuiQqsCount);
-            mSysuiQqsCount
-                    .setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+            int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
+            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
+            return true;
+       } else if (preference == mRowsPortrait) {
+            intValue = Integer.valueOf((String) objValue);
+            index = mRowsPortrait.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_ROWS_PORTRAIT, intValue);
+            preference.setSummary(mRowsPortrait.getEntries()[index]);
+            return true;
+        } else if (preference == mColumnsPortrait) {
+            intValue = Integer.valueOf((String) objValue);
+            index = mColumnsPortrait.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_COLUMNS_PORTRAIT, intValue);
+            preference.setSummary(mColumnsPortrait.getEntries()[index]);
+            return true;
+        } else if (preference == mRowsLandscape) {
+            intValue = Integer.valueOf((String) objValue);
+            index = mRowsLandscape.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_ROWS_LANDSCAPE, intValue);
+            preference.setSummary(mRowsLandscape.getEntries()[index]);
+            return true;
+        } else if (preference == mColumnsLandscape) {
+            intValue = Integer.valueOf((String) objValue);
+            index = mColumnsLandscape.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_COLUMNS_LANDSCAPE, intValue);
+            preference.setSummary(mColumnsLandscape.getEntries()[index]);
             return true;
         }
         return false;
