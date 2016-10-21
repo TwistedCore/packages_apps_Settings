444bcac03578be87c52c84ed70e614305b159fe2
rogersb11 <brettrogers11@gmail.com>
2016-10-21 15:07:23 +0200

    Settings: The Return of the Ticker [1/2]
    Change-Id: Ia8c3dff1a30f9106032b8dca0fcd9b4d5455ef4f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    # Conflicts:
    #	res/values/custom_arrays.xml
    #	res/xml/notifications_settings.xml
    #	src/com/android/settings/twisted/quicksettings/QuickSettings.java

--- src/com/android/settings/twisted/statusbar/StatusbarSettings.java
+++ src/com/android/settings/twisted/statusbar/StatusbarSettings.java
@@ -43,6 +43,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import com.android.settings.preference.CustomSeekBarPreference;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
@@ -50,8 +51,19 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 public class StatusbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+    private static final String SHOW_TICKER = "status_bar_show_ticker";
+    private static final String CAT_COLORS = "ticker_colors";
+    private static final String TEXT_COLOR = "status_bar_ticker_text_color";
+    private static final String ICON_COLOR ="status_bar_ticker_icon_color";
+    private static final String STATUS_BAR_TICKER_FONT_STYLE = "status_bar_ticker_font_style";
+    private static final String STATUS_BAR_TICKER_FONT_SIZE  = "status_bar_ticker_font_size";
 
     private SwitchPreference mEnableNC;
+    private SwitchPreference mShowTicker;
+    private ColorPickerPreference mTextColor;
+    private ColorPickerPreference mIconColor;
+    private CustomSeekBarPreference mTickerFontSize;
+    private ListPreference mTickerFontStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,6 +79,43 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements OnP
         int EnableNC = Settings.System.getInt(getContentResolver(),
                 STATUS_BAR_NOTIF_COUNT, 0);
         mEnableNC.setChecked(EnableNC != 0);
+
+        int intColor;
+        String hexColor;
+
+        mShowTicker = (SwitchPreference) findPreference(SHOW_TICKER);
+        mShowTicker.setChecked(Settings.System.getInt(resolver,
+                "status_bar_show_ticker", 0) == 1);
+        mShowTicker.setOnPreferenceChangeListener(this);
+
+        mTextColor = (ColorPickerPreference) findPreference(TEXT_COLOR);
+        mTextColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getContentResolver(),
+                "status_bar_ticker_text_color", 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mTextColor.setSummary(hexColor);
+        mTextColor.setNewPreviewColor(intColor);
+        mTextColor.setAlphaSliderVisible(true);
+
+        mIconColor = (ColorPickerPreference) findPreference(ICON_COLOR);
+        mIconColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(resolver,
+                "status_bar_ticker_icon_color", 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mIconColor.setSummary(hexColor);
+        mIconColor.setNewPreviewColor(intColor);
+        mIconColor.setAlphaSliderVisible(true);
+
+        mTickerFontSize = (CustomSeekBarPreference) findPreference(STATUS_BAR_TICKER_FONT_SIZE);
+        mTickerFontSize.setValue(Settings.System.getInt(resolver,
+                      Settings.System.STATUS_BAR_TICKER_FONT_SIZE, 14));
+        mTickerFontSize.setOnPreferenceChangeListener(this);
+
+        mTickerFontStyle = (ListPreference) findPreference(STATUS_BAR_TICKER_FONT_STYLE);
+        mTickerFontStyle.setOnPreferenceChangeListener(this);
+        mTickerFontStyle.setValue(Integer.toString(Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_TICKER_FONT_STYLE, 0)));
+        mTickerFontStyle.setSummary(mTickerFontStyle.getEntry());
     }
 
     @Override
@@ -86,6 +135,39 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements OnP
             boolean value = (Boolean) newValue;
             Settings.System.putInt(resolver, STATUS_BAR_NOTIF_COUNT,
                     value ? 1 : 0);
+            return true;
+        } else if (preference == mShowTicker) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                   Settings.System.STATUS_BAR_SHOW_TICKER, value ? 1 : 0);
+            return true;
+        } else if (preference == mTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                    "status_bar_ticker_text_color", intHex);
+            return true;
+        } else if (preference == mIconColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                    "status_bar_ticker_icon_color", intHex);
+            return true;
+        } else if (preference == mTickerFontSize) {
+             int width = ((Integer)newValue).intValue();
+              Settings.System.putInt(resolver,
+                      Settings.System.STATUS_BAR_TICKER_FONT_SIZE, width);
+              return true;
+        } else if (preference == mTickerFontStyle) {
+            int val = Integer.parseInt((String) newValue);
+            int index = mTickerFontStyle.findIndexOfValue((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_TICKER_FONT_STYLE, val);
+            mTickerFontStyle.setSummary(mTickerFontStyle.getEntries()[index]);
             return true;
         }
         return false;
