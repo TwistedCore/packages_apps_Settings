bba3fe9f5b505ee53416fdf6ae3995bdcacee324
bigrushdog <randall.rushing@gmail.com>
2016-10-28 21:41:01 -0700

    DUI: Initial N checkin
    Change-Id: If8d277d124e2844810482a52bfb951549ee8c999
    
    # Conflicts:
    #	res/values/custom_strings.xml
    #	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -135,6 +135,10 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
+import com.flash.settings.fragments.FlingSettings;
+import com.flash.settings.fragments.NavbarSettings;
+import com.flash.settings.fragments.SmartbarSettings;
+import com.flash.settings.fragments.PulseSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -363,6 +367,10 @@ public class SettingsActivity extends SettingsDrawerActivity
             AutomaticStorageManagerSettings.class.getName(),
             TwistedCoreSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 
