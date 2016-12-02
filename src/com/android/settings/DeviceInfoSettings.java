998eb0bbece65ed4ed2a4749624570dd4f7d6872
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-12-02 02:17:45 +0100

    Settings: Show toolchains used for compilation (Clang and GCC) [2/2]
    # Conflicts:
    #	res/values/custom_strings.xml
    #	res/xml/device_info_settings.xml

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -65,6 +65,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_ROM_VERSION = "rom_version";
     private static final String KEY_VENDOR_VERSION = "vendor_version";
+    private static final String KEY_MOD_BUILD_COMPILER_GCC = "build_compiler_gcc";
+    private static final String KEY_MOD_BUILD_COMPILER_CLANG = "build_compiler_clang";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -123,6 +125,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         findPreference(KEY_KERNEL_VERSION).setSummary(DeviceInfoUtils.getFormattedKernelVersion());
         setValueSummary(KEY_ROM_VERSION, "ro.rom.version");
         findPreference(KEY_ROM_VERSION).setEnabled(true);
+        setValueSummary(KEY_MOD_BUILD_COMPILER_GCC, "ro.build.gcc");
+        setValueSummary(KEY_MOD_BUILD_COMPILER_CLANG, "ro.build.clang");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
