2184e56cf2c6e5e4cb4dcbc460eb0a90247034f8
aow1980 <wilkes.allen@gmail.com>
2016-12-16 23:40:20 -0500

    Create a Qs Tiles fragment and shuffle those settings from statusbar into it
    (cherry picked from commit 6bd02e47bb639c957deca8b8725d172234558858)
    
    # Conflicts:
    #	res/xml/statusbar_settings.xml
    #	src/com/android/settings/twisted/StatusBarSettings.java

--- src/com/android/settings/twisted/StatusBarSettings.java
+++ src/com/android/settings/twisted/StatusBarSettings.java
@@ -50,16 +50,8 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 public class StatusBarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
-    private static final String PREF_COLUMNS = "qs_layout_columns";
-    private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
-    private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
-    private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
 
     private SwitchPreference mEnableNC;
-    private CustomSeekBarPreference mQsColumns;
-    private CustomSeekBarPreference mRowsPortrait;
-    private CustomSeekBarPreference mRowsLandscape;
-    private CustomSeekBarPreference mSysuiQqsCount;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,31 +67,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
         int EnableNC = Settings.System.getInt(getContentResolver(),
                 STATUS_BAR_NOTIF_COUNT, 0);
         mEnableNC.setChecked(EnableNC != 0);
-
-        mQsColumns = (CustomSeekBarPreference) findPreference(PREF_COLUMNS);
-        int columnsQs = Settings.System.getInt(resolver,
-                Settings.System.QS_LAYOUT_COLUMNS, 3);
-        mQsColumns.setValue(columnsQs / 1);
-        mQsColumns.setOnPreferenceChangeListener(this);
-
-        mRowsPortrait = (CustomSeekBarPreference) findPreference(PREF_ROWS_PORTRAIT);
-        int rowsPortrait = Settings.System.getInt(resolver,
-                Settings.System.QS_ROWS_PORTRAIT, 3);
-        mRowsPortrait.setValue(rowsPortrait / 1);
-        mRowsPortrait.setOnPreferenceChangeListener(this);
-
-        mSysuiQqsCount = (CustomSeekBarPreference) findPreference(PREF_SYSUI_QQS_COUNT);
-        int SysuiQqsCount = Settings.Secure.getInt(getContentResolver(),
-                Settings.Secure.QQS_COUNT, 6);
-        mSysuiQqsCount.setValue(SysuiQqsCount / 1);
-        mSysuiQqsCount.setOnPreferenceChangeListener(this);
-
-        int defaultValue = getResources().getInteger(com.android.internal.R.integer.config_qs_num_rows_landscape_default);
-        mRowsLandscape = (CustomSeekBarPreference) findPreference(PREF_ROWS_LANDSCAPE);
-        int rowsLandscape = Settings.System.getInt(resolver,
-                Settings.System.QS_ROWS_LANDSCAPE, defaultValue);
-        mRowsLandscape.setValue(rowsLandscape / 1);
-        mRowsLandscape.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -119,22 +86,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements OnP
             boolean value = (Boolean) newValue;
             Settings.System.putInt(resolver, STATUS_BAR_NOTIF_COUNT,
                     value ? 1 : 0);
-            return true;
-        } else if (preference == mQsColumns) {
-            int qsColumns = (Integer) newValue;
-            Settings.System.putInt(resolver, Settings.System.QS_LAYOUT_COLUMNS, qsColumns * 1);
-            return true;
-        } else if (preference == mRowsPortrait) {
-            int rowsPortrait = (Integer) newValue;
-            Settings.System.putInt(resolver, Settings.System.QS_ROWS_PORTRAIT, rowsPortrait * 1);
-           return true;
-        } else if (preference == mSysuiQqsCount) {
-            int SysuiQqsCount = (Integer) newValue;
-            Settings.Secure.putInt(resolver, Settings.Secure.QQS_COUNT, SysuiQqsCount * 1);
-            return true;
-        } else if (preference == mRowsLandscape) {
-            int rowsLandscape = (Integer) newValue;
-            Settings.System.putInt(resolver, Settings.System.QS_ROWS_LANDSCAPE, rowsLandscape * 1);
             return true;
         }
         return false;
---
+++ src/com/android/settings/twisted/TilesSettings.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2015-2016 The Dirty Unicorns Project
+ * Copyright (C) 2016 The Pure Nexus Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.twisted;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.Vibrator;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.android.settings.preference.CustomSeekBarPreference;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class TilesSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+
+    private static final String PREF_COLUMNS = "qs_layout_columns";
+    private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
+    private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
+    private static final String PREF_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
+
+    private CustomSeekBarPreference mQsColumns;
+    private CustomSeekBarPreference mRowsPortrait;
+    private CustomSeekBarPreference mRowsLandscape;
+    private CustomSeekBarPreference mSysuiQqsCount;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.tiles_settings);
+
+        final PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mQsColumns = (CustomSeekBarPreference) findPreference(PREF_COLUMNS);
+        int columnsQs = Settings.System.getInt(resolver,
+                Settings.System.QS_LAYOUT_COLUMNS, 3);
+        mQsColumns.setValue(columnsQs / 1);
+        mQsColumns.setOnPreferenceChangeListener(this);
+
+        mRowsPortrait = (CustomSeekBarPreference) findPreference(PREF_ROWS_PORTRAIT);
+        int rowsPortrait = Settings.System.getInt(resolver,
+                Settings.System.QS_ROWS_PORTRAIT, 3);
+        mRowsPortrait.setValue(rowsPortrait / 1);
+        mRowsPortrait.setOnPreferenceChangeListener(this);
+
+        mSysuiQqsCount = (CustomSeekBarPreference) findPreference(PREF_SYSUI_QQS_COUNT);
+        int SysuiQqsCount = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.QQS_COUNT, 6);
+        mSysuiQqsCount.setValue(SysuiQqsCount / 1);
+        mSysuiQqsCount.setOnPreferenceChangeListener(this);
+
+        int defaultValue = getResources().getInteger(com.android.internal.R.integer.config_qs_num_rows_landscape_default);
+        mRowsLandscape = (CustomSeekBarPreference) findPreference(PREF_ROWS_LANDSCAPE);
+        int rowsLandscape = Settings.System.getInt(resolver,
+                Settings.System.QS_ROWS_LANDSCAPE, defaultValue);
+        mRowsLandscape.setValue(rowsLandscape / 1);
+        mRowsLandscape.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.TWISTED;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+         if (preference == mQsColumns) {
+            int qsColumns = (Integer) newValue;
+            Settings.System.putInt(resolver, Settings.System.QS_LAYOUT_COLUMNS, qsColumns * 1);
+            return true;
+        } else if (preference == mRowsPortrait) {
+            int rowsPortrait = (Integer) newValue;
+            Settings.System.putInt(resolver, Settings.System.QS_ROWS_PORTRAIT, rowsPortrait * 1);
+           return true;
+        } else if (preference == mSysuiQqsCount) {
+            int SysuiQqsCount = (Integer) newValue;
+            Settings.Secure.putInt(resolver, Settings.Secure.QQS_COUNT, SysuiQqsCount * 1);
+            return true;
+        } else if (preference == mRowsLandscape) {
+            int rowsLandscape = (Integer) newValue;
+            Settings.System.putInt(resolver, Settings.System.QS_ROWS_LANDSCAPE, rowsLandscape * 1);
+            return true;
+        }
+        return false;
+    }
+}
